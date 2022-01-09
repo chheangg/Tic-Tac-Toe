87cccbf522b94c90e8bc87e7dc0877b4925975d8
chheangg <balldremz@gmail.com>
2022-01-10 00:28:08 +0700

    Refactor code and add bot mode

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 const double = document.querySelector('.double');
+const bot = document.querySelector('.bot')
 
 // Player factory Function
 const players = function(name, turnID, side) {
@@ -8,40 +9,53 @@ const players = function(name, turnID, side) {
 
 // create Two Players and assign them a side
 const playersList = (function() {
-    let playerOne;
-    let playerTwo;
-
     // find a randomside for player one
-    const randSide = function() {
-        let randValue = Math.floor(Math.random() * 2);
-        let side;
-        if (randValue == 0) {
-            side = 'X';
+    
+    const _randSide = function() {
+        if (Math.floor(Math.random() * 2) == 0) {
+            return 'X';
         } else {
-            side = 'O';
+            return 'O';
         }
-        return side;
     }
     // find a side for player two based on player one
-    const autoSide = function() {
+    const _autoSide = function() {
         if ( playerOne.side == 'X' ) {
             return 'O';
         } else if (playerOne.side == 'O') {
             return 'X';
         }
     }
+    
+    const _chanceX = function() {
+        if (Math.floor(Math.random() * 2) == 0) {
+            return '0';
+        } else {
+            return '1';
+        }
+    }
+
+    const _chanceY = function() {
+        if ( playerOne.turnID == '1' ) {
+            return '0';
+        } else if (playerOne.turnID == '0') {
+            return '1';
+        }
+    }
+
     const _autoSet = function() {
-        playerOne = players('Player One', 0, null);
-        playerTwo = players('Player Two', 1, null);
-        playerOne.side = randSide();
-        playerTwo.side = autoSide();
+        playerOne = players('Player One', _chanceX(), null);
+        playerTwo = players('Player Two', _chanceY(), null);
+        playerOne.side = _randSide();
+        playerTwo.side = _autoSide();
     }
 
     const _reset = function() {
-        playerOne.turnID = 0;
-        playerTwo.turnID = 1;
-        playerOne.side = randSide();
-        playerTwo.side = autoSide();
+        playerOne.turnID = _chanceX();
+        playerTwo.turnID = _chanceY();
+        playerOne.side = _randSide();
+        playerTwo.side = _autoSide();
+        gameController.turnNumber = 0;
     }
 
     _autoSet();
@@ -63,33 +77,29 @@ const gameBoard = (function() {
 
 // Module that controls the displaying of content
 const displayController = (function() {
-
+    let isWin = false;
     // produce a winning screen, and reset the game + array
     const winScreen = function(winner, winnerSide) {
-        const modal = document.querySelector('.modal-bg');
-        const button = document.querySelector('.continue');
-        const text = document.querySelector('.modal-container p');
-        text.textContent = `${winner} has won the game! (Side ${winnerSide})`
-
-        modal.style.display = "flex";
-        double.style.backgroundColor = "whitesmoke";
-        button.addEventListener('click', clearBoard);
+        console.log(winner);
+        document.querySelector('.modal-bg').style.display = "flex";
+        document.querySelector('.continue').addEventListener('click', _clearBoard);
+        document.querySelector('.modal-container p').textContent = `${winner} has won the game! (Side ${winnerSide})`;
+        displayController.isWin = true;
     }
 
     // produce a tie screen, and reset the game + array
     const tieScreen = function() {
-        const modal = document.querySelector('.modal-bg');
-        const button = document.querySelector('.continue');
-        const text = document.querySelector('.modal-container p');
-        text.textContent = `It's a tie :(`;
-
-        modal.style.display = "flex";
-        double.style.backgroundColor = "whitesmoke";
-        button.addEventListener('click', clearBoard);
+        document.querySelector('.modal-bg').style.display = "flex";
+        document.querySelector('.continue').addEventListener('click', _clearBoard);
+        document.querySelector('.modal-container p').textContent = "It's a tie :(";
+        displayController.isWin = true;
     }
 
     // loop through each board array and display them
     const displayBoard = function() {
+        document.querySelector('.buttons').removeChild(double);
+        document.querySelector('.buttons').removeChild(bot);
+        let container = document.querySelector('.box-container')
         for ( value in gameBoard ) {
             let column = 0;
             gameBoard[value].forEach((object) => {
@@ -100,14 +110,19 @@ const displayController = (function() {
                 objectPlacement.setAttribute('data-key', `${value + column}`);
                 objectPlacement.textContent = object;
 
-                let container = document.querySelector('.box-container');
                 box.appendChild(objectPlacement);
                 container.appendChild(box);
                 column += 1;
             })
         }
     }
-    const clearBoard = function() {
+    const redisplayBoard = function() {
+        document.querySelectorAll('.box-content').forEach((box) => {
+            let boxID = box.getAttribute('data-key').split("");
+            box.textContent = gameBoard[boxID[0]][[boxID[1]]];
+        })
+    }
+    const _clearBoard = function() {
         document.querySelector('.modal-bg').style.display = "none";
         for ( value in gameBoard ) {
             for ( i = 0; i < gameBoard[value].length; i++) {
@@ -118,13 +133,14 @@ const displayController = (function() {
         boxes.forEach((box) => {
             box.textContent = null;
         })
-
+        botPlayer.botMode();
     }
-    return {displayBoard, winScreen, tieScreen};
+    return {displayBoard, redisplayBoard, winScreen, tieScreen, isWin};
 })();
 
 // Main module that controls the game and its function
 const gameController = (function() {
+    let turnNumber = 0;
     const rowBoard = function() {
         return gameBoard;
     }
@@ -134,71 +150,145 @@ const gameController = (function() {
     const _checkPlayer = function(winValue) {
         playersList.forEach((player) => {
             if ( player.side == winValue) {
+                console.log(player);
                 displayController.winScreen(player.name, player.side);
             }
         })
-        displayController.winScreen(player.name, player.side);
     }
-    const _evaluateRound = function() {
+    const evaluateRound = function() {
         let board = rowBoard();
         for ( value in board ) {
             if (board[value][0] == board[value][1] && board[value][0] == board[value][2] && board[value][0] != null) {
-                _checkPlayer(board[i]);
+                console.log(board[0]);
+                _checkPlayer(board[value][0]);
             }
         }
         board = arrayBoard();
         for ( i = 0; i < board.length; i++ ) {
-            if (board[i] == board[i+3] && board[i] == board[i+6] && board[i] != null) {
+            if (board[i] == board[i+3] && board[i] == board[i+6] && (board[i] != null || board[i+3] != null)) {
+                console.log(board[i]);
                 _checkPlayer(board[i]);
                 break;
-            } else if (board[i] == board[i+4] && board[i] == board[i+8] && board[i] != null) {
+            } else if (board[i] == board[i+4] && board[i] == board[i+8] && (board[i] != null || board[i+4] != null)) {
+                console.log(board[i]);
                 _checkPlayer(board[i]);
                 break;
             } else if (board[2] == board[4] && board[2] == board[6] && board[2] != null) {
-                _checkPlayer(board[i]);
+                console.log(board[i]);
+                _checkPlayer(board[2]);
                 break;
-            } else if ( board.includes(null) == false ) {
+            } else if ( i == 8 && board[8] != null && board.includes(null) == false ) {
                 displayController.tieScreen();
                 break;
             }
         }
     } 
     const boxListener = function() {
+        if ( botPlayer.isBotPlaying == true) {
+        } else {
+             displayController.displayBoard();
+        }
         double.style.backgroundColor = "silver";
         const boxes = document.querySelectorAll('.box-content');
-        boxes.forEach((box) => {
-            box.addEventListener('click', _addContent);
-            box.addEventListener('click', _evaluateRound);
-        })
+            boxes.forEach((box) => {
+                box.addEventListener('click', addContent);
+            })
     };
+
     //add content if detected click
     //don't add them if already filled
     //gameBoard array mirrored
-    const _addContent = function(box) {
-        const boxes = document.querySelectorAll('.box-content');
-        const boxID = box.target.getAttribute('data-key');
-        for ( player of playersList ) {
-                if ( box.target.textContent != '') {
-                    return;
-                }
-                if ( player.turnID == 0 ) {
-                    gameBoard[boxID.charAt(0)][boxID.charAt(1)] = player.side;
-                    box.target.textContent = player.side;
-                    //Changes the turnID which decide the move for next round
-                    player.turnID = 1;
-                    playersList.forEach((otherPlayer) => {
-                        if ( otherPlayer.side != player.side ) {
-                            otherPlayer.turnID = 0;
+    const addContent = function(box) {
+        if ( Array.isArray(box) ) {
+            for ( player of playersList) {
+                if ( player.turnID == botPlayer.botTurnID ) {
+                    if ( gameBoard[box[0]][box[1]] != null) {
+                        return;
+                    }
+                   //change turns number
+                    if ( displayController.isWin == false ) {
+                        gameBoard[box[0]][box[1]] = player.side; 
+                        displayController.redisplayBoard();
+                        gameController.evaluateRound()
+                        if ( displayController.isWin == true) {
+                            displayController.isWin = false;
+                            gameController.turnNumber = 0;
+                            break;
                         }
-                    })
-                    break;
-                } 
-        };
-    };
+                        if ( gameController.turnNumber == 0) {
+                            ++gameController.turnNumber;
+                        } else if ( gameController.turnNumber == 1 ) {
+                            --gameController.turnNumber;
+                        }
+                    } 
+                        break;
+                }
+            }
+        } else {
+            const boxes = document.querySelectorAll('.box-content');
+            const boxID = box.target.getAttribute('data-key');
+            for ( player of playersList ) {
+                    if ( box.target.textContent != '') {
+                        return;
+                    } else if ( player.turnID == gameController.turnNumber ) {
+                        gameBoard[boxID.charAt(0)][boxID.charAt(1)] = player.side;
+                        displayController.redisplayBoard();
+                                                //change turns number
+                        if ( displayController.isWin == false ) {
+                            gameController.evaluateRound()
+                            if ( displayController.isWin == true) {
+                                displayController.isWin = false;
+                                gameController.turnNumber = 0;
+                                break;
+                            }
+                            if ( gameController.turnNumber == 0) {
+                                ++gameController.turnNumber;
+                            } else if ( gameController.turnNumber == 1 ) {
+                                --gameController.turnNumber;
+                            }
+                            if ( botPlayer.isBotPlaying == true) {
+                                botPlayer.botInput();
+                            }
+                        } 
+                        break;
+                    } 
+            };
+        }
 
-    displayController.displayBoard();
-    return {boxListener};
+
+
+    };
+    return {boxListener, addContent, evaluateRound, turnNumber};
 
 })();
 
+const botPlayer = (function() {
+    let botTurnID = (() => { return playersList[1].turnID })();
+    let isBotPlaying = false;
+
+    function botEvaluate() {
+            let bestRow = Math.floor(Math.random() * 3);
+            let bestColumn = Math.floor(Math.random() * 3);
+        return [bestRow, bestColumn];
+    }
+
+    function botInput() {
+        if ( gameController.turnNumber == botTurnID) {
+            let bestChoice = botEvaluate();
+            if ( gameBoard[bestChoice[0]][bestChoice[1]] != null ) {
+                botInput();
+            } else {
+                gameController.addContent(bestChoice);
+            }
+        }
+    }
+    function botMode() {
+        gameController.boxListener();
+        botPlayer.isBotPlaying = true;
+        botInput();
+    }
+    return {botMode, botInput, botEvaluate, isBotPlaying, botTurnID}
+})()
+
 double.addEventListener('click', gameController.boxListener);
+bot.addEventListener('click', botPlayer.botMode)
